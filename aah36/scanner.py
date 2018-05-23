cfd717235cf186cafd86ad76c12850df336abcb2
Arsalan <arsalanharris@gmail.com>
2018-05-23 22:11:24 +0100

    scanner updated, testscan is how it was called, ex_def_file is one of the examples in the back of the report with the changes that were made that I used to test it

--- aah36/scanner.py
+++ aah36/scanner.py
@@ -34,68 +34,67 @@ class Scanner:
 
     def __init__(self, path, names):
         """Open specified file and initialise reserved words and IDs."""
-        try:
+        """try:
             input_file = open(path, 'r')
         except FileNotFoundError:
             print("File doesn't exist")
-            sys.exit()
+            sys.exit()"""
 
-        re.sub('/[^>]+/', '', input_file)
+        self.input_file = open(path, 'r')
+
+        #re.sub('/[^>]+/', '', input_file)
 
         self.names = names
         self.symbol_type_list = [self.COMMA, self.SEMICOLON, self.COLON,
-                                 self.KEYWORD, self.NUMBER, self.NAME, self.ARROW] = range(7)
+                                 self.KEYWORD, self.NUMBER, self.NAME, self.ARROW, self.EOF, self.NEWLINE] = range(9)
         self.keywords_list = ["DEVICES", "CONNECTIONS", "MONITOR", "DTYPE", "XOR", "AND", "NAND", "OR", "NOR", "SWITCH",
                               "CLOCK"]
-        [self.DEVICES_ID, self.CONNECTIONS_ID, self.MONITOR_ID] = self.names.lookup(self.keywords_list)
+        dummy = self.names.lookup(self.keywords_list)
+        #[self.DEVICES_ID, self.CONNECTIONS_ID, self.MONITOR_ID] = self.names.lookup(self.keywords_list)
         self.current_character = ""
+        self.name_string = ''
+        self.advance()
+        self.skip_spaces()
+
 
     def advance(self):
-        char = input_file.read(1)
+        char = self.input_file.read(1)
         self.current_character = char
         return char
 
     def skip_spaces(self):
-        while True:
-            char = input_file.read(1)
-            if not char.isspace():
-                self.current_character = char
-                return char
-            else:
-                continue
+        while self.current_character.isspace():
+            self.current_character = self.input_file.read(1)
 
     def get_name(self):
         name = ''
-        while True:
-            char = input_file.read(1)
-            if char == '':
-                break
-            if char.isalpha():
-                name = char
-                break
+        if self.current_character.isalpha():
+            name = self.current_character
 
         while True:
-            nextchar = input_file.read(1)
-            if nextchar.isalnum() == True:
+            nextchar = self.input_file.read(1)
+            if nextchar.isalnum():
                 name = name + nextchar
                 continue
             else:
-                self.current_character = next_char
+                self.current_character = nextchar
                 break
         return name
 
     def get_number(self):
         number = ""
-        while True:
-            char = file.read(1)
+        if self.current_character.isdigit():
+            number = self.current_character
+        """while True:
+            char = self.input_file.read(1)
             if char == '':
                 break
             if char.isdigit():
                 number = char
-                break
+                break"""
 
         while True:
-            nextchar = file.read(1)
+            nextchar = self.input_file.read(1)
             if nextchar.isdigit() == True:
                 number = number + nextchar
                 continue
@@ -111,49 +110,64 @@ class Scanner:
         if it encounters a comment or end of line.
         """
 
-        def get_symbol(self):
-            """Return the symbol type and ID of the next sequence of characters."""
 
-            self.skip_spaces()  # current character now not whitespace
+        """Return the symbol type and ID of the next sequence of characters."""
 
-            if self.current_character.isalpha():  # name
-                name_string = self.get_name()
+          # current character now not whitespace
+        #print(self.name_string)
+        if self.current_character.isalpha():  # name
+            self.name_string = self.get_name()
 
-                if name_string in self.keywords_list:
-                    symbol_type = self.KEYWORD
+            if self.name_string in self.keywords_list:
+                symbol_type = self.KEYWORD
+                symbol_id = self.names.query(self.name_string)
 
-                else:
-                    symbol_type = self.NAME
-                    [symbol_id] = self.names.lookup([name_string])
+            else:
 
-            elif self.current_character.isdigit():  # number
-                symbol_id = self.get_number()
-                symbol_type = self.NUMBER
+                symbol_type = self.NAME
+                [symbol_id] = self.names.lookup([self.name_string])
 
-            elif self.current_character == ",":
-                symbol_type = self.COMMA
-                symbol_id = None
-                self.advance()
+        elif self.current_character.isdigit():  # number
+            symbol_id = self.get_number()
+            symbol_type = self.NUMBER
 
-            elif self.current_character == ";":
-                symbol_type = self.SEMICOLON
-                symbol_id = None
-                self.advance()
+        elif self.current_character == ",":
+            symbol_type = self.COMMA
+            symbol_id = None
+            self.advance()
 
-            elif self.current_character == ":":
-                symbol_type = self.COLON
-                symbol_id = None
-                self.advance()
+        elif self.current_character == ";":
+            symbol_type = self.SEMICOLON
+            symbol_id = None
+            self.advance()
 
-            elif self.current_character == "-":
-                symbol_type = self.ARROW
-                symbol_id = None
-                self.advance()
-                self.advance()
+        elif self.current_character == ":":
+            symbol_type = self.COLON
+            symbol_id = None
+            self.advance()
 
-            else:  # not a valid character
-                symbol_type = None
-                symbol_id = None
-                self.advance()
+        elif self.current_character == '\n':
+            symbol_type = self.NEWLINE
+            symbol_id = None
+            self.advance()
 
-            return [symbol_type, symbol_id]
+        elif self.current_character == "-":
+            symbol_type = self.ARROW
+            symbol_id = None
+            self.advance()
+            self.advance()
+
+        elif self.current_character == "":
+            symbol_type = self.EOF
+            symbol_id = None
+
+        else:  # not a valid character
+            symbol_type = None
+            symbol_id = None
+            self.advance()
+
+        self.skip_spaces()
+
+        return [symbol_type, symbol_id]
+
+
