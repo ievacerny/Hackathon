1f0cba6509e3b459d26457d46521d04e383fe8b9
Arsalan <arsalanharris@gmail.com>
2018-05-23 12:45:02 +0100

    scanner almost done - not finished comment removal and couple of other things

--- aah36/scanner.py
+++ aah36/scanner.py
@@ -8,6 +8,8 @@ Classes
 Scanner - reads definition file and translates characters into symbols.
 """
 
+import re
+import sys
 
 class Scanner:
 
@@ -32,6 +34,74 @@ class Scanner:
 
     def __init__(self, path, names):
         """Open specified file and initialise reserved words and IDs."""
+        try:
+            input_file = open(path, 'r')
+        except FileNotFoundError:
+            print("File doesn't exist")
+            sys.exit()
+
+        re.sub('/[^>]+/', '', input_file)
+
+        self.names = names
+        self.symbol_type_list = [self.COMMA, self.SEMICOLON, self.COLON,
+                                 self.KEYWORD, self.NUMBER, self.NAME, self.ARROW] = range(7)
+        self.keywords_list = ["DEVICES", "CONNECTIONS", "MONITOR", "DTYPE", "XOR", "AND", "NAND", "OR", "NOR", "SWITCH",
+                              "CLOCK"]
+        [self.DEVICES_ID, self.CONNECTIONS_ID, self.MONITOR_ID] = self.names.lookup(self.keywords_list)
+        self.current_character = ""
+
+    def advance(self):
+        char = input_file.read(1)
+        self.current_character = char
+        return char
+
+    def skip_spaces(self):
+        while True:
+            char = input_file.read(1)
+            if not char.isspace():
+                self.current_character = char
+                return char
+            else:
+                continue
+
+    def get_name(self):
+        name = ''
+        while True:
+            char = input_file.read(1)
+            if char == '':
+                break
+            if char.isalpha():
+                name = char
+                break
+
+        while True:
+            nextchar = input_file.read(1)
+            if nextchar.isalnum() == True:
+                name = name + nextchar
+                continue
+            else:
+                self.current_character = next_char
+                break
+        return name
+
+    def get_number(self):
+        number = ""
+        while True:
+            char = file.read(1)
+            if char == '':
+                break
+            if char.isdigit():
+                number = char
+                break
+
+        while True:
+            nextchar = file.read(1)
+            if nextchar.isdigit() == True:
+                number = number + nextchar
+                continue
+            else:
+                self.current_character = nextchar
+                break
 
     def get_symbol(self):
         """Return the symbol type and ID of the next sequence of characters.
@@ -40,3 +110,50 @@ class Scanner:
         are assigned None. Note: this function is called again (recursively)
         if it encounters a comment or end of line.
         """
+
+        def get_symbol(self):
+            """Return the symbol type and ID of the next sequence of characters."""
+
+            self.skip_spaces()  # current character now not whitespace
+
+            if self.current_character.isalpha():  # name
+                name_string = self.get_name()
+
+                if name_string in self.keywords_list:
+                    symbol_type = self.KEYWORD
+
+                else:
+                    symbol_type = self.NAME
+                    [symbol_id] = self.names.lookup([name_string])
+
+            elif self.current_character.isdigit():  # number
+                symbol_id = self.get_number()
+                symbol_type = self.NUMBER
+
+            elif self.current_character == ",":
+                symbol_type = self.COMMA
+                symbol_id = None
+                self.advance()
+
+            elif self.current_character == ";":
+                symbol_type = self.SEMICOLON
+                symbol_id = None
+                self.advance()
+
+            elif self.current_character == ":":
+                symbol_type = self.COLON
+                symbol_id = None
+                self.advance()
+
+            elif self.current_character == "-":
+                symbol_type = self.ARROW
+                symbol_id = None
+                self.advance()
+                self.advance()
+
+            else:  # not a valid character
+                symbol_type = None
+                symbol_id = None
+                self.advance()
+
+            return [symbol_type, symbol_id]
